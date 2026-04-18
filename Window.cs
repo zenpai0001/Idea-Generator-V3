0b17bc769af0bb3272cda22437bd1fe92148f59d
zenpai0001 <zenpai0001@gmail.com>
2026-04-18 13:25:53 -0400

    Enhance MainWindow with reusable UI components
    Refactor MainWindow class to include reusable components and initialize UI elements..

--- Window.cs
+++ Window.cs
@@ -1,10 +1,13 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using Terminal.Gui;
 using Terminal.Gui.App;
 using Terminal.Gui.Configuration;
 using Terminal.Gui.Drawing;
+
+using Terminal.Gui.ViewBase;
+
 using Terminal.Gui.Views;
 
 namespace Idea_Generator_V3
@@ -18,20 +21,63 @@ namespace Idea_Generator_V3
                 ConfigurationManager.RuntimeConfig = """{ "Theme": "Amber Phosphor" }""";
                 ConfigurationManager.Enable(ConfigLocations.All);
 
+                
+
                 app.Run<MainWindow>().GetResult<string>();
+
             }
         }
     }
     public sealed class MainWindow : Runnable<string>
     {
-    public void Button()
-    {
-        var button = new Button();
-    }
-    public void Label()
-    {
-        var label = new Label();
-    }
+
+        //This is a bit complicated to be honest but it is neccessary.
+        public Terminal.Gui.Views.Window? drawWindow;
+
+        public Terminal.Gui.FileServices.IFileOperations? fileOperations;
+
+        //Adding component fields for reusability.
+        public View GetView;
+
+        public Label GetLabel;
+
+        public Button button;
+
+        
+        //This can be overloaded later on.
+        public MainWindow()
+        {
+            Console.Title = "Idea Generator";
+
+            drawWindow = new Terminal.Gui.Views.Window
+            {
+                Title = "Window",
+                Arrangement = ViewArrangement.Fixed,
+                Width = Dim.Auto()
+            };
+
+            button = new Button
+            {
+                Text = "Button",
+                X = 5,
+                Y = 10,
+                Width = Dim.Fill()
+            };
+            button = new Button
+            {
+                Text = "Text",
+                X= button.X + 5,
+                Y = button.Y + 0,
+                Width = Dim.Fill()
+            };
+
+            
+
+            
+            Add(drawWindow,button);
+
+            GetView?.ScrollVertical(10);
+        }
         
     }
 }
