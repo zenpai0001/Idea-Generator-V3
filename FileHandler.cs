3c863f09e57862b5fbec1d079a3224ed0ca3549b
zenpai0001 <zenpai0001@gmail.com>
2026-04-23 00:40:07 -0400

    Added async operation to the filehandler
    Updated GetData method to use async IAsyncEnumerable for asynchronous file reading. Added Randomizer class for randomizing text from file data. Work in progress at the moment.

--- FileHandler.cs
+++ FileHandler.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 using Terminal.Gui;
+using Terminal.Gui.ViewBase;
 
 namespace Idea_Generator_V3
 {
@@ -10,18 +12,44 @@ namespace Idea_Generator_V3
     {
         public string? filePath {get; set;}
 
-      
         //Lazy load the lines from the filePath provided.
-        public static IEnumerable<string> GetData(string filePath)
+        public static async IAsyncEnumerable<string> GetData(string filePath)
         {
             string line;
             using (var reader = new StreamReader(filePath)) 
             {
                 while ((line = reader.ReadLine()) != null)
                 {
-                    yield return line;
+                    await foreach (var item in GetData(filePath))
+                    {
+                        yield return line;
+                    }
+ 
                 }
             }
+        }
+        public class Randomizer
+        {
+            private static Random _random = new Random();
+            private FileHandler _fileHandler = new FileHandler();
+            private IAsyncEnumerator<string> _dataEnumerator;
+
+            public void RandomizeText()
+            {
+                //Get the data from the file and randomize it.
+                _dataEnumerator = GetData(_fileHandler.filePath).GetAsyncEnumerator();
+
+                /*
+                 * Work in progress, this is a bit tricky to be honest but I think I can make it work.
+                foreach (var item in _dataEnumerator(_fileHandler.filePath))
+                {
+                    int index = _random.Next(0, item.Length);
+                    string randomLine = item[index];
+                    Console.WriteLine(randomLine);
+                }*/
+
+            }
+
         }
         
     }
