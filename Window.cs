1756cdb1f2c514f997d47dee56b8977422e99e09
zenpai0001 <zenpai0001@gmail.com>
2026-04-23 00:43:06 -0400

    Adding new elements to the main window.
    Refactored MainWindow constructor to improve GUI element initialization and layout.

--- Window.cs
+++ Window.cs
@@ -1,11 +1,16 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
 using System.Text;
 using Terminal.Gui;
 using Terminal.Gui.App;
 using Terminal.Gui.Configuration;
 using Terminal.Gui.Drawing;
-
+using Terminal.Gui.Drivers;
+using Terminal.Gui.Input;
+using Terminal.Gui.Testing;
+using Terminal.Gui.Time;
 using Terminal.Gui.ViewBase;
 
 using Terminal.Gui.Views;
@@ -21,10 +26,7 @@ namespace Idea_Generator_V3
                 ConfigurationManager.RuntimeConfig = """{ "Theme": "Amber Phosphor" }""";
                 ConfigurationManager.Enable(ConfigLocations.All);
 
-                
-
                 app.Run<MainWindow>().GetResult<string>();
-
             }
         }
     }
@@ -36,48 +38,55 @@ namespace Idea_Generator_V3
 
         public Terminal.Gui.FileServices.IFileOperations? fileOperations;
 
-        //Adding component fields for reusability.
-        public View GetView;
-
-        public Label GetLabel;
-
-        public Button button;
-
         
         //This can be overloaded later on.
         public MainWindow()
         {
+            //Draw the GUI elements here
+
             Console.Title = "Idea Generator";
 
             drawWindow = new Terminal.Gui.Views.Window
             {
                 Title = "Window",
                 Arrangement = ViewArrangement.Fixed,
+                BorderStyle = LineStyle.Rounded,
+                Width = Dim.Fill(),
+                Height = Dim.Fill()
+            };
+
+            var button = new Button
+            {
+                Text = "Button",
+                Width = Dim.Auto(),
+                Height = Dim.Auto(),
+                BorderStyle = LineStyle.Rounded
+            };
+
+
+            var label = new Label()
+            {
+                Text = "Label",
+                Width = Dim.Auto(),
+                Height = Dim.Auto(),
+                BorderStyle = LineStyle.Rounded
+            };
+
+            var bar = new Bar()
+            {
+                AssignHotKeys = true,
+                BorderStyle = LineStyle.Rounded,
                 Width = Dim.Auto()
             };
 
-            button = new Button
-            {
-                Text = "Button",
-                X = 5,
-                Y = 10,
-                Width = Dim.Fill()
-            };
-            button = new Button
-            {
-                Text = "Text",
-                X= button.X + 5,
-                Y = button.Y + 0,
-                Width = Dim.Fill()
-            };
+            bar.AddShortcutAt(0, new MenuItem());
 
+            drawWindow.Add(label);
             
 
-            
-            Add(drawWindow,button);
+            label.Add(bar);
 
-            GetView?.ScrollVertical(10);
+            Add(drawWindow);
         }
-        
     }
 }
