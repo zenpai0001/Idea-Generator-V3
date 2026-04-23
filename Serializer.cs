2691f66acade8e023216bde43bc550b0bae2a6a1
zenpai0001 <zenpai0001@gmail.com>
2026-04-23 00:38:20 -0400

    Working on serializing JSON
    Added methods for serializing and deserializing JSON in the serializer class. Work in progress.

--- Serializer.cs
+++ Serializer.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using Newtonsoft.Json;
@@ -8,6 +8,34 @@ namespace Idea_Generator_V3
     
     public class Serializer
     {
+        //Basic serializer class to convert the generated idea into a JSON format.
+
+        [JsonProperty("id")]
+        public int generatedID;
+
+        [JsonProperty("text")]
+        public Dictionary<string, string> text;
+
+        public Serializer(int id, Dictionary<string, string> text)
+        {
+            this.generatedID = id;
+            this.text = text;
+        }
+        public void SerializeToJson(string filePath)
+        {
+            string json = JsonConvert.SerializeObject(this, Formatting.Indented);
+            filePath = @"C:\Users\zenpa\source\repos\Idea Generator V3\Data.json";
+            System.IO.File.WriteAllText(filePath, json);
+        }
+        public void DeserializeFromJson(string filePath)
+        {
+            Serializer deserializedObject = new Serializer(0, null);
+            string json = System.IO.File.ReadAllText(filePath);
+            filePath = @"C:\Users\zenpa\source\repos\Idea Generator V3\Data.json";
+            deserializedObject = JsonConvert.DeserializeObject<Serializer>(json);
+            generatedID = deserializedObject.generatedID;
+            text = deserializedObject.text;
+        }
 
     }
 }
